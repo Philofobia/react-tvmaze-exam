adb920576e132fc88e5e331c9124bcb3d0340e2e
MarcoParisi <marco.parisi@edu.itspiemonte.it>
2022-11-20 21:51:45 +0100

    set  up database

--- src/Pages/Favourite/Favourite.tsx
+++ src/Pages/Favourite/Favourite.tsx
@@ -1,6 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addShow, removeShow } from "../../redux/reducers/favourite.slice";
+import { RootState } from "../../redux/store/store";
+import CardComponent from "../../shared/Card";
+
+import { CurrentUserConsumer } from "../../context/AuthContext";
+import { getUserShows } from "../../services/firebase.db";
+
 const FavouritePage = () => {
+  const { currentUser } = CurrentUserConsumer();
+  const favShows = useSelector((state: RootState) => {
+    return state.favShow;
+  });
+  const dispatch = useDispatch();
+
+  const handleUserShow = () => {
+    if (currentUser) {
+      getUserShows(currentUser.uid, (Math.random())*10);
+    }
+  };
+
   return (
-    <h2 className="font-title text-2xl antialiasing my-5 text-center">MY FAV SHOWS</h2>
+    <>
+      <h2 className="font-title text-2xl antialiasing my-5 text-center">
+        MY FAV SHOWS
+      </h2>
+      <button className="btn" onClick={handleUserShow}>
+        SI
+      </button>
+      <button className="btn" onClick={() => dispatch(addShow)}>
+        NO
+      </button>
+      <div className="flex flex-col flex-wrap md:flex-row">
+        {favShows &&
+          favShows.map((show, index) => (
+            <CardComponent show={show} key={index} />
+          ))}
+      </div>
+    </>
   );
 };
 
--- src/redux/reducers/favourite.slice.ts
+++ src/redux/reducers/favourite.slice.ts
@@ -1,19 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { searchMovieBool } from "../../services/models";
 
-export interface favSlice {
-    favShows: searchMovieBool[];
-}
-const initalState = {
-    favShows: []
-}
+export type favSlice = searchMovieBool[];
+const initalState: searchMovieBool[] = [];
 
 export const favouriteSlice = createSlice({
-    name: "favourite shows",
+    name: "favouriteShows",
     initialState: initalState,
     reducers: {
         addShow: () => {
-
+            
         },
         removeShow: () => {
 
