4310e71c0a1821458fa0644ce416fc1a4f42f646
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2022-12-11 22:27:11 +0100

    404 page graphics

--- src/Pages/PageNotFound/PageNotFound.tsx
+++ src/Pages/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,30 @@
-const PageNotFound = () => {
-    return <h2>Page Not Found 404</h2>
-}
 
-export default PageNotFound
+import { Link } from "react-router-dom";
+const PageNotFound = () => {
+  return (
+    <section className="flex items-center h-full p-16">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="font-title text-2xl font-semibold md:text-3xl">
+            Sorry, we couldn't find this page.
+          </p>
+          <p className="font-body mt-4 mb-8">
+            But dont worry, you can find plenty of other things on our homepage.
+          </p>
+          <Link
+            rel="noopener noreferrer"
+            to="/home"
+            className="px-8 py-3 font-semibold rounded btn"
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PageNotFound;
