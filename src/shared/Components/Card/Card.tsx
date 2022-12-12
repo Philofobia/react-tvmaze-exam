c116ca773c2ca51a233c6d7858d85e0d30376a5a
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2022-12-12 12:10:28 +0100

    cards graphics

--- src/shared/Components/Card/Card.tsx
+++ src/shared/Components/Card/Card.tsx
@@ -38,8 +38,8 @@ const CardComponent = (props: propsMovieCard) => {
   };
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl m-3">
-      <figure className="max-h-64">
+    <div className="card w-72 bg-base-100 shadow-xl m-3 sm:w-96">
+      <figure className="max-h-32 sm:max-h-64">
         <img
           src={props.show.show.image?.medium || posterPlaceholder}
           alt={props.show.show.name}
@@ -47,13 +47,61 @@ const CardComponent = (props: propsMovieCard) => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          {props.show.show.name}
-          <div className="badge badge-secondary">
+        <div className="card-title">
+          <h3 className="font-title text-xl antialiasing">
+            {props.show.show.name}
+          </h3>
+          <div className="font-body badge badge-secondary ml-auto">
             {props.show.show.language}
           </div>
+          <div className="hidden max-h-14 w-36 sm:inline-block">
+            <button
+              className="btn w-12 ml-3"
+              onClick={() => handleShowFav(props.show)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill={favourite ? "black" : "none"}
+                viewBox="2 2 20 20"
+                stroke="black"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                />
+              </svg>
+            </button>
+            <button
+              className="btn w-12 ml-3 align-bottom"
+              onClick={() => handleShowWatch(props.show)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill={watchingCheck ? "black" : "none"}
+                viewBox="1 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="card-title mx-auto sm:hidden">
           <button
-            className="btn"
+            className="btn w-12"
             style={{ width: 50, marginLeft: 10 }}
             onClick={() => handleShowFav(props.show)}
           >
@@ -72,7 +120,7 @@ const CardComponent = (props: propsMovieCard) => {
             </svg>
           </button>
           <button
-            className="btn"
+            className="btn w-12"
             style={{ width: 50, marginLeft: 10 }}
             onClick={() => handleShowWatch(props.show)}
           >
@@ -96,22 +144,25 @@ const CardComponent = (props: propsMovieCard) => {
               />
             </svg>
           </button>
-        </h2>
-        <div className="p-1 max-h-32" style={{ overflow: "hidden" }}>
+        </div>
+        <Link to={"/details/" + props.show.show.id} className="btn w-full">
+          READ MORE
+        </Link>
+        <div
+          className="font-body antialiasing p-1 max-h-32 hidden sm:inline-block"
+          style={{ overflow: "hidden" }}
+        >
           {props.show.show.summary
             ? parse(`${props.show.show.summary}`)
             : "NO INFO"}
         </div>
-        <div className="card-actions justify-end">
+        <div className="font-body card-actions justify-center sm:justify-end">
           {props.show.show.genres?.map((genre, i) => (
             <div className="badge badge-outline" key={i}>
               {genre}
             </div>
           ))}
         </div>
-        <Link to={"/details/" + props.show.show.id}>
-          <button className="btn">READ MORE</button>
-        </Link>
       </div>
     </div>
   );
